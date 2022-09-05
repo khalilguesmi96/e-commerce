bc83beea94c7a9f48f1f50ffd72a3369ea08e808
khalilguesmi96 <khalil.guesmi96@gmail.com>
2022-09-05 05:42:09 +0100

    first commit

--- src/e_commerce/admin.py
+++ src/e_commerce/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import item
+from .models import Item, OrderItem, Order
 # Register your models here.
-admin.site.register(item)
+admin.site.register(Item)
+admin.site.register(OrderItem)
+admin.site.register(Order)
--- src/e_commerce/models.py
+++ src/e_commerce/models.py
@@ -1,8 +1,25 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
-class item(models.Model):
-    name = models.CharField(max_length=120)
+class Item(models.Model):
+    title = models.CharField(max_length=120)
+    price = models.FloatField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+class Order(models.Model):
+    use = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    def __str__(self):
+        return self.use.username
