bc33ef1595ac2fbce059b2969b2b40224b2a7777
khalilguesmi96 <khalil.guesmi96@gmail.com>
2022-09-08 15:15:38 +0100

    first commit

--- src/e_commerce/models.py
+++ src/e_commerce/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
+CATEGORY_CHOICES = (
+    ('S','Shirt'),
+    ('SW','Sport Wear'),
+    ('OW','Outwear'),
+
+)
+LABEL_CHOICES = (
+    ('P','primary'),
+    ('S','secondary'),
+    ('D','danger'),
+
+)
+
+
 class Item(models.Model):
     title = models.CharField(max_length=120)
     price = models.FloatField(default=0)
-
+    image = models.ImageField(null=True,blank=True,upload_to='image')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     def __str__(self):
         return self.title
 
