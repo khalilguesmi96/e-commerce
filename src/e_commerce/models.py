482fd40e891a426546e28156aa1e18062b5abc8f
khalilguesmi96 <khalil.guesmi96@gmail.com>
2022-09-10 18:24:46 +0100

    first commit

--- src/e_commerce/models.py
+++ src/e_commerce/models.py
@@ -19,11 +19,12 @@ LABEL_CHOICES = (
 class Item(models.Model):
     title = models.CharField(max_length=120)
     price = models.FloatField(default=0)
-    image = models.ImageField(null=True,blank=True,upload_to='image')
+    image = models.ImageField(null=True,blank=True,upload_to='images')
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     def __str__(self):
         return self.title
+
 
 class OrderItem(models.Model):
     item = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
