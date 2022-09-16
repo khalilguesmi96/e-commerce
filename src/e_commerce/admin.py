f67e33293e9319c6131582e3818534dea06c15b1
khalilguesmi96 <khalil.guesmi96@gmail.com>
2022-09-16 18:53:46 +0100

    first commit

--- src/e_commerce/admin.py
+++ src/e_commerce/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Item, OrderItem, Order
 # Register your models here.
+class AdminItem(admin.ModelAdmin):
+        list_display = ('title', 'price', 'image', 'category', 'label')
+        list_display_links = ['title']
+        search_fields = ['title', 'price', 'category']
+        list_filter = ['category', 'label']
 
 
-admin.site.register(Item)
+class AdminOrder(admin.ModelAdmin):
+    list_display = ('use', 'start_date', 'ordered_date', 'ordered')
+    list_display_links = ['use']
+    search_fields = ['use', 'items', 'ordered']
+    list_filter = ['start_date', 'ordered_date']
+
+
+admin.site.register(Item,AdminItem)
 admin.site.register(OrderItem)
-admin.site.register(Order)
+admin.site.register(Order,AdminOrder)
